017943bfffa23a4d55fbbca7ff4493428f7d59d6
Ryder Spees <ryderspees@gmail.com>
2022-05-14 12:33:28 -0400

    Update input method

--- SudokuSolver/SudokuSolver.cpp
+++ SudokuSolver/SudokuSolver.cpp
@@ -12,6 +12,14 @@ int findValidNum(zeroIndex selectedIndex, int selectedNum, int grid[9][9]);
 
 int stepCounter = 0;
 
+void readInput(int (&grid)[9][9]) {
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            cin >> grid[i][j];
+        }
+    }
+}
+
 int main()
 {
 
@@ -23,20 +31,9 @@ int main()
     zeroIndex selectedIndex;
     vector< pair<int, int> > indexHolder;
 
+    int grid[9][9];
 
-
-    /* ------MODIFY THIS GRID WITH ANY SUDOKU BOARD------ */
-    int grid[9][9] = { {3, 0, 6, 5, 0, 8, 4, 0, 0},
-     {5, 2, 0, 0, 0, 0, 0, 0, 0},
-     {0, 8, 7, 0, 0, 0, 0, 3, 1},
-     {0, 0, 3, 0, 1, 0, 0, 8, 0},
-     {9, 0, 0, 8, 6, 3, 0, 0, 5},
-     {0, 5, 0, 0, 9, 0, 6, 0, 0},
-     {1, 3, 0, 0, 0, 0, 2, 5, 0},
-     {0, 0, 0, 0, 0, 0, 0, 7, 4},
-     {0, 0, 5, 2, 0, 6, 3, 0, 0} };
-
-
+    readInput(grid);
 
     // print the original array
     printGrid(grid);
